7e3acedac5cfbad3726896cfa373d5ea86cbdea1
Thamsanqa Qaba <thamie.qaba@gmail.com>
2024-11-07 15:15:15 +0200

    temp syntax update.js

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ console.log(`'}`);
 // if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 
 let currentTemp = 4; // Current temperature in °C
-currentTemp >= 5 ? "Chilled to perfection" : "Needs a cooler!"
-console.log(currentTemp);
+let Tempmessage = currentTemp <= 5 ? "Chilled to perfection" : "Needs a cooler!"
+console.log(Tempmessage);
 
 
 // #### Challenge 5: Late Night Coding Session
