036fba55a11be96c9f99f0d9a68d82c90759b70d
Thamsanqa Qaba <thamie.qaba@gmail.com>
2024-11-07 11:44:21 +0200

    temperoary solutions.js

--- index.js
+++ index.js
@@ -5,14 +5,16 @@
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
+userPreference = 'Sugar-free' ? "Sugar-free" : "Regular" : (userPreference = 'Sugar-free') ? 'undefined';
+console.log(userPreference);
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
 let cansLeft = 3; // Any number of cans
-console.log(``);
+cansLeft >=3 ? "Time to restock!" : "We're stocked!"
+console.log(cansLeft);
 
 
 // #### Challenge 3: Workout Intensity
@@ -20,6 +22,7 @@ console.log(``);
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
 
 let heartRate = 95; // Current heart rate in bpm
+heartRate <=100 ? "Boost needed!" : "Energy levels are high!"
 console.log(`'}`);
 
 
